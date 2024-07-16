71c1d42bacf53d12c14b923b8852daf60c051456
ChengTing <aaronzhan0906@gmail.com>
2024-07-16 22:56:52 +0800

    chore: Clean up code in app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
-import asyncio
-
 from routers import api_router
 from routers.websockets import router as websocket_router, handle_messages
+import asyncio
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,20 +17,13 @@ async def lifespan(app: FastAPI):
     except asyncio.CancelledError:
         pass
 
+
 app = FastAPI(lifespan=lifespan)
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 app.include_router(api_router)
 app.include_router(websocket_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/websockets", include_in_schema=False)
 async def websockets(request: Request):
     return FileResponse("./static/websockets.html", media_type="text/html")
-
-app.include_router(api_router)
-
-
-@app.get("/websockets", include_in_schema=False)
-async def websockets(request: Request):
-	return FileResponse("./static/websockets.html", media_type="text/html")
